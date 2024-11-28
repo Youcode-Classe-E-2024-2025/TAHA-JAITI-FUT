8e35abb871e00eeb8ad207bfb6b84222807263fd
Taha <kyojin.rar@gmail.com>
2024-11-28 20:23:03 +0100

    fixed some redundancy in my code stuff that would break the code, improved readability and fixed bugs

--- public/main.js
+++ public/main.js
@@ -70,7 +70,7 @@ const fetchData = async () => {
 //     });
 // };
 
-const formationContainer = document.getElementById('#formationContainer');
+const formationContainer = document.getElementById('formationContainer');
 const insertContainer = document.getElementById('insertContainer');
 const allPlayers = document.querySelector('#allPlayersContainer');
 const plrDisplay = document.getElementById('plrDisplay');
@@ -82,6 +82,8 @@ const openAdd = document.getElementById('openAdd');
 const closeAll = document.querySelector('#closeAll');
 const openAll = document.getElementById('openAll');
 const closeDisplay = document.querySelector('#closeDisplay');
+
+const insertBtn = document.querySelector('#insertBtn');
 const closeInsert = document.querySelector("#closeInsert");
 
 const emptyCard = document.querySelectorAll('.emptyCard');
@@ -98,7 +100,7 @@ const proceedBtn = msgContainer.querySelector('#proceedMsg');
 
 let posArray = [];
 
-
+//inputs for adding
 const addInputs = {
     name: document.querySelector('#nameInput'),
     nationality: document.querySelector('#natioInput'),
@@ -125,16 +127,7 @@ const addInputs = {
     rating: document.querySelector('#ratingInput'),
 };
 
-addInputs.position.addEventListener('change', (e) => {
-    const value = e.target.value
-    addInputs.pace.previousElementSibling.textContent = value === 'GK' ? 'Diving' : 'Pace';
-    addInputs.shooting.previousElementSibling.textContent = value === 'GK' ? 'Handling' : 'Shooting';
-    addInputs.passing.previousElementSibling.textContent = value === 'GK' ? 'Kicking' : 'Passing';
-    addInputs.dribbling.previousElementSibling.textContent = value === 'GK' ? 'Reflexes' : 'Dribbling';
-    addInputs.defending.previousElementSibling.textContent = value === 'GK' ? 'Speed' : 'Defending';
-    addInputs.physical.previousElementSibling.textContent = value === 'GK' ? 'Positioning' : 'Physical';
-});
-
+//displaying player values
 const displayValues = {
     name: document.querySelector('.displayName'),
     position: document.querySelector('.displayPos'),
@@ -156,6 +149,89 @@ const displayValues = {
     flag: document.querySelector('.displayFlag'),
     photo: document.querySelector('.displayPhoto'),
     rating: document.querySelector('.displayRating'),
+};
+
+closeDisplay.addEventListener('click', () => {
+    closeDisplay.parentElement.parentElement.classList.toggle('hidden');
+});
+
+closeAdd.addEventListener('click', () => {
+    addForm.parentElement.classList.toggle('hidden');
+});
+
+openAdd.addEventListener('click', () => {
+    addForm.parentElement.classList.toggle('hidden');
+})
+
+closeAll.addEventListener('click', () => {
+    allPlayers.parentElement.parentElement.classList.toggle('hidden');
+});
+
+openAll.addEventListener('click', () => {
+    loadPlayers(data, allPlayers);
+    allPlayers.parentElement.parentElement.classList.toggle('hidden');
+});
+
+closeInsert.addEventListener('click', () => {
+    insertContainer.parentElement.parentElement.classList.toggle('hidden');
+});
+
+//accomodating for gk inputs
+addInputs.position.addEventListener('change', (e) => {
+    const value = e.target.value
+    addInputs.pace.previousElementSibling.textContent = value === 'GK' ? 'Diving' : 'Pace';
+    addInputs.shooting.previousElementSibling.textContent = value === 'GK' ? 'Handling' : 'Shooting';
+    addInputs.passing.previousElementSibling.textContent = value === 'GK' ? 'Kicking' : 'Passing';
+    addInputs.dribbling.previousElementSibling.textContent = value === 'GK' ? 'Reflexes' : 'Dribbling';
+    addInputs.defending.previousElementSibling.textContent = value === 'GK' ? 'Speed' : 'Defending';
+    addInputs.physical.previousElementSibling.textContent = value === 'GK' ? 'Positioning' : 'Physical';
+});
+
+//loadplayers into a container
+const loadPlayers = (players, container) => {
+    container.innerHTML = "";
+    if (players.length === 0) {
+        container.innerHTML = '<p class="text-2xl">No players exist.</p>'
+    } else {
+        players.forEach(player => {
+            const stats = player.position === 'GK'
+                ? `
+                        <p>DIV ${player.diving}</p>
+                        <p>HAN ${player.handling}</p>
+                        <p>KIC ${player.kicking}</p>
+                        <p>REF ${player.reflexes}</p>
+                        <p>SPE ${player.speed}</p>
+                        <p>POS ${player.positioning}</p>
+                      `
+                : `
+                        <p>PAC ${player.pace}</p>
+                        <p>SHO ${player.shooting}</p>
+                        <p>DRI ${player.dribbling}</p>
+                        <p>PAS ${player.passing}</p>
+                        <p>DEF ${player.defending}</p>
+                        <p>PHY ${player.physical}</p>
+                      `;
+
+            container.innerHTML += `
+                    <!-- PLAYER CARD -->
+                    <div data-id="${player.id}" data-pos="${player.position}" class="player notSelected bg-gold-card m-0 text-black">
+                        <div class="w-fit font-semibold absolute top-6 left-2">
+                            <p>${player.rating}</p>
+                            <p>${player.position}</p>
+                        </div>
+                        <img class="h-1/2 mt-7" src="${player.photo}" alt="">
+                        <p>${player.name}</p>
+                        <div class="flex text-xs gap-1">
+                            ${stats}
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <img class="h-4" src="${player.flag}" alt="">
+                            <img class="h-5 object-fill" src="${player.logo}" alt="">
+                        </div>
+                    </div>
+                `;
+        });
+    }
 };
 
 //dynamic msg display using callback
@@ -181,31 +257,6 @@ const displayMsg = (msg, color, confirm, callback) => {
     };
 };
 
-closeDisplay.addEventListener('click', () => {
-    closeDisplay.parentElement.parentElement.classList.toggle('hidden');
-});
-
-closeAdd.addEventListener('click', () => {
-    addForm.parentElement.classList.toggle('hidden');
-});
-
-openAdd.addEventListener('click', () => {
-    addForm.parentElement.classList.toggle('hidden');
-})
-
-closeAll.addEventListener('click', () => {
-    allPlayers.parentElement.parentElement.classList.toggle('hidden');
-});
-
-openAll.addEventListener('click', async () => {
-    await loadPlayers(data, allPlayers);
-    allPlayers.parentElement.parentElement.classList.toggle('hidden');
-});
-
-closeInsert.addEventListener('click', () => {
-    insertContainer.parentElement.parentElement.classList.toggle('hidden');
-});
-
 //add button
 document.querySelector('#addBtn').addEventListener('click', async (e) => {
     e.preventDefault();
@@ -219,7 +270,7 @@ document.querySelector('#addBtn').addEventListener('click', async (e) => {
     Object.entries(addInputs).forEach(([key, input]) => {
         let errorText = null;
         const validateInputs = () => {
-            if (!input.value && input.type !== 'file' && !key === 'id') {
+            if (!input.value && input.type !== 'file' && key !== 'id') {
                 return `${key} can't be empty.`;
             }
             if (key === 'name' && !/^[a-zA-Z\s]{1,20}$/.test(input.value)) {
@@ -279,6 +330,7 @@ document.querySelector('#addBtn').addEventListener('click', async (e) => {
     }
 });
 
+//converting images to base64
 function convertToBase64(file) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -288,106 +340,26 @@ function convertToBase64(file) {
     });
 };
 
-//loadplayers into a container
-const loadPlayers = (players, container) => {
-    container.innerHTML = "";
-    if (players.length === 0) {
-        container.innerHTML = '<p class="text-2xl">No players exist.</p>'
-    } else {
-        players.forEach(player => {
-            const stats = player.position === 'GK'
-                ? `
-                        <p>DIV ${player.diving}</p>
-                        <p>HAN ${player.handling}</p>
-                        <p>KIC ${player.kicking}</p>
-                        <p>REF ${player.reflexes}</p>
-                        <p>SPE ${player.speed}</p>
-                        <p>POS ${player.positioning}</p>
-                      `
-                : `
-                        <p>PAC ${player.pace}</p>
-                        <p>SHO ${player.shooting}</p>
-                        <p>DRI ${player.dribbling}</p>
-                        <p>PAS ${player.passing}</p>
-                        <p>DEF ${player.defending}</p>
-                        <p>PHY ${player.physical}</p>
-                      `;
-
-            container.innerHTML += `
-                    <!-- PLAYER CARD -->
-                    <div data-id="${player.id}" data-pos="${player.position}" class="player notSelected bg-gold-card m-0 text-black">
-                        <div class="w-fit font-semibold absolute top-6 left-2">
-                            <p>${player.rating}</p>
-                            <p>${player.position}</p>
-                        </div>
-                        <img class="h-1/2 mt-7" src="${player.photo}" alt="">
-                        <p>${player.name}</p>
-                        <div class="flex text-xs gap-1">
-                            ${stats}
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <img class="h-4" src="${player.flag}" alt="">
-                            <img class="h-5 object-fill" src="${player.logo}" alt="">
-                        </div>
-                    </div>
-                `;
-        });
-    }
-};
-
 let selectedPlayer;
 let targetCard; //the target card position
-let currTarget; //variable for the currenttarget that comes from empty card
+let currTarget; //the card container that holds position data
 let displayedPlr;
 
-//event listners for each empty card
-emptyCard.forEach((card) => {
-    card.addEventListener('click', async (e) => {
-        e.preventDefault();
-        currTarget = e.currentTarget;
-        if (e.target.dataset.pos) { targetCard = e.target.dataset.pos; }
-
-        loading.classList.toggle('hidden');
-        posArray.length = 0;
-        posArray = fetchExistingPlayers(targetCard);
-        loadPlayers(posArray, insertContainer);//load players into the container
-
-        setTimeout(() => {
-            loading.classList.add('hidden');
-            insertContainer.parentElement.parentElement.classList.remove('hidden');// display container
-        }, 100);
-
-        //selection
-        const playerCards = insertContainer.querySelectorAll('.notSelected');
-        playerCards.forEach((playerCard) => {
-            playerCard.addEventListener('click', () => {
-                playerCards.forEach((card) => card.classList.remove('selectedCard'));
-                playerCard.classList.add('selectedCard');
-                selectedPlayer = playerCard.dataset.id;
-            });
-        });
-
-        const insertBtn = document.querySelector('#insertBtn');
-        insertBtn.addEventListener('click', applyInsert);
-    });
-});
 
 //check for existing players with the same pos
 const fetchExistingPlayers = (targetPos) => {
     let arr = [];
     data.forEach((players) => {
-        let existingCard = document.querySelector(`#plr${players.id}`);
+        let existingCard = document.getElementById(`plr${players.id}`);
         if (existingCard) {
             return;
         }
-        if (players.position === targetPos || currTarget.dataset.sub) {
+        if (players.position === targetPos && !existingCard || currTarget.dataset.sub) {
             arr.push(players);
         }
     });
     return arr;
-}
-
-let inTeam = document.querySelectorAll('.inTeam');
+};
 
 //applying the insertion
 const applyInsert = (e) => {
@@ -415,7 +387,7 @@ const applyInsert = (e) => {
                       `;
 
     //fill the container target html
-    currTarget.classList.remove('bg-card');
+    currTarget.classList.remove('bg-card', 'emptyCard');
     currTarget.innerHTML = `
                 <!-- PLAYER CARD -->
                 <div id="plr${playerData.id}" data-id="${playerData.id}" data-pos="${playerData.position}" class="player inTeam bg-gold-card m-0 text-black">
@@ -436,54 +408,139 @@ const applyInsert = (e) => {
         `
     insertContainer.parentElement.parentElement.classList.toggle('hidden');
 
-
     //adding displayplr eventlistners
-    inTeam = document.querySelectorAll('.inTeam');
-    inTeam.forEach((player) => {
-        player.addEventListener('click', (event) => {
-            event.stopPropagation();
+    const inTeam = document.querySelectorAll('.inTeam');
 
-            displayedPlr = data.find(plr => String(plr.id) === event.currentTarget.dataset.id);
-            console.log(displayedPlr);
-            currTarget = event.currentTarget.parentElement;
+    // inTeam.forEach((player) => {
+    //     const cleanPlayer = player.cloneNode(true);
+    //     player.replaceWith(cleanPlayer);
 
-            const shortStat = {
-                pace: 'PAC',
-                shooting: 'SHO',
-                dribbling: 'DRI',
-                passing: 'PAS',
-                defending: 'DEF',
-                physical: 'PHY',
+    //     cleanPlayer.addEventListener('click', (event) => {
+    //         event.stopPropagation();
+    //         const playerId = cleanPlayer.dataset.id;
+    //         displayedPlr = data.find(plr => String(plr.id) === playerId);
+    //         console.log(displayedPlr);
 
-                diving: 'DIV',
-                handling: 'HAN',
-                kicking: 'KIC',
-                reflexes: 'REF',
-                speed: 'SPE',
-                positioning: 'POS',
-            };
+    //         currTarget = cleanPlayer.parentElement;
 
-            Object.keys(displayValues).forEach(key => {
-                if (Object.keys(shortStat).includes(key)) {
-                    if (displayedPlr.position === 'GK') {
-                        displayValues[key].textContent = `${shortStat[key]} ${displayedPlr[key]}`;
-                    }
-                } else if (key === 'photo' || key === 'flag' || key === 'logo') {
-                    displayValues[key].src = displayedPlr[key];
-                } else {
-                    displayValues[key].textContent = displayedPlr[key];
-                }
-            });
+    //         const shortStat = {
+    //             pace: 'PAC',
+    //             shooting: 'SHO',
+    //             dribbling: 'DRI',
+    //             passing: 'PAS',
+    //             defending: 'DEF',
+    //             physical: 'PHY',
+    //             diving: 'DIV',
+    //             handling: 'HAN',
+    //             kicking: 'KIC',
+    //             reflexes: 'REF',
+    //             speed: 'SPE',
+    //             positioning: 'POS',
+    //         };
 
-            plrDisplay.classList.remove('hidden');
-        })
-    });
+    //         Object.keys(displayValues).forEach(key => {
+    //             if (Object.keys(shortStat).includes(key)) {
+    //                 if (displayedPlr.position === 'GK') {
+    //                     displayValues[key].textContent = `${shortStat[key]} ${displayedPlr[key]}`;
+    //                 }
+    //             } else if (key === 'photo' || key === 'flag' || key === 'logo') {
+    //                 displayValues[key].src = displayedPlr[key];
+    //             } else {
+    //                 displayValues[key].textContent = displayedPlr[key];
+    //             }
+    //         });
+
+    //         plrDisplay.classList.remove('hidden');
+    //     });
+    // });
+
 };
+
+//event listeners for players that are in team so we can display data
+formationContainer.addEventListener('click', (event) => {
+    const playerElement = event.target.closest('.inTeam');
+    if (!playerElement) return;
+    event.stopPropagation();
+
+    const playerId = playerElement.dataset.id;
+    displayedPlr = data.find(plr => String(plr.id) === playerId);
+    console.log(displayedPlr);
+
+    currTarget = playerElement.parentElement;
+    console.log('containerplr: ',currTarget);
+
+    const shortStat = {
+        pace: 'PAC',
+        shooting: 'SHO',
+        dribbling: 'DRI',
+        passing: 'PAS',
+        defending: 'DEF',
+        physical: 'PHY',
+        diving: 'DIV',
+        handling: 'HAN',
+        kicking: 'KIC',
+        reflexes: 'REF',
+        speed: 'SPE',
+        positioning: 'POS',
+    };
+
+    // Update the display values
+    Object.keys(displayValues).forEach(key => {
+        if (Object.keys(shortStat).includes(key)) {
+            if (displayedPlr.position === 'GK') {
+                displayValues[key].textContent = `${shortStat[key]} ${displayedPlr[key]}`;
+            }
+        } else if (key === 'photo' || key === 'flag' || key === 'logo') {
+            displayValues[key].src = displayedPlr[key];
+        } else {
+            displayValues[key].textContent = displayedPlr[key];
+        }
+    });
+
+    // Unhide the player display modal
+    plrDisplay.classList.remove('hidden');
+});
+
+//event listeners for emptycards so we can insert a player
+formationContainer.addEventListener('click', (e) => {
+    const card = e.target.closest('.emptyCard');
+    if (!card) return;
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    currTarget = card;
+    if (e.target.dataset.pos) {
+        targetCard = e.target.dataset.pos;
+    }
+
+    loading.classList.toggle('hidden');
+    posArray.length = 0;
+    posArray = fetchExistingPlayers(targetCard);
+    loadPlayers(posArray, insertContainer);
+    setTimeout(() => {
+        loading.classList.add('hidden');
+        insertContainer.parentElement.parentElement.classList.remove('hidden');
+    }, 100);
+
+    // selection of player cards
+    const playerCards = insertContainer.querySelectorAll('.notSelected');
+    playerCards.forEach((playerCard) => {
+        playerCard.addEventListener('click', () => {
+            playerCards.forEach((card) => card.classList.remove('selectedCard'));
+            playerCard.classList.add('selectedCard');
+            selectedPlayer = playerCard.dataset.id;
+        });
+    });
+
+    //applying insert
+    insertBtn.addEventListener('click', applyInsert);
+});
 
 //remove plr
 removePlr.addEventListener('click', (e) => {
     e.stopPropagation();
-    currTarget.classList.add('bg-card');
+    currTarget.classList.add('bg-card', 'emptyCard');
     currTarget.innerHTML = `<span class="icon-[gg--add] text-4xl text-lime-green ">
                                 </span><p class="font-bold">${displayedPlr.position}</p>`;
     plrDisplay.classList.add('hidden');
@@ -494,14 +551,14 @@ changePlr.addEventListener('click', (e) => {
     e.stopPropagation();
 
     //fetch for players based on the position of the displayed plr
-    fetchExistingPlayers(displayedPlr.position);
+    posArray.length = 0;
+    posArray = fetchExistingPlayers(displayedPlr.position);
     loadPlayers(posArray, insertContainer);
     plrDisplay.classList.add('hidden');
 
     const insertPlr = insertContainer.querySelectorAll('.notSelected');
 
     insertPlr.forEach((plr) => {
-        console.log(plr);
         plr.addEventListener('click', () => {
             console.log('clicked');
             insertPlr.forEach((card) => card.classList.remove('selectedCard'));
@@ -554,4 +611,4 @@ searchInput.addEventListener('keyup', (e) => {
     }
 });
 
-document.addEventListener('DOMContentLoaded', fetchData)
+document.addEventListener('DOMContentLoaded', fetchData);
