652ecff90d4b21557d57d61c7c9ea4ad9f8f467b
Taha <kyojin.rar@gmail.com>
2024-11-29 22:06:55 +0100

    working on responsivity for cards and containers, made modals responsive

--- public/util/convertToBase64.js
+++
@@ -1,9 +0,0 @@
-//conversion to base64
-export function convertToBase64(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsDataURL(file);
-    });
-}
