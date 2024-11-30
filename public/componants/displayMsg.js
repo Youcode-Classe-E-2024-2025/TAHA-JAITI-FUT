1c364737e0ede93c1b444e775844d1ea35b6c078
Taha <kyojin.rar@gmail.com>
2024-11-30 22:39:57 +0100

    changed the displaymsg to use the display module for smoother animations

--- public/componants/displayMsg.js
+++ public/componants/displayMsg.js
@@ -1,27 +1,29 @@
+import { display } from "./displayHandler.js";
+
 const msgContainer = document.getElementById('msgContainer');
 const msgText = msgContainer.querySelector('#msgDisplay');
 const closeMsg = msgContainer.querySelector('#closeMsg');
 const proceedBtn = msgContainer.querySelector('#proceedMsg');
 
 //dynamic msg display using callback
-export const displayMsg = (msg, color, confirm, callback) => {
-    msgContainer.parentElement.classList.remove('hidden');
+export const displayMsg = (msg, color, confirm = null, callback) => {
+    display('open',msgContainer.parentElement); //displaying the msg
     msgText.textContent = msg;
-    msgText.classList = `text-center text-${color}-400`;
+    msgText.classList = `text-center text-${color}-400`; //color depending on the color sent
 
-    if (confirm) {
+    if (confirm) { //if there is a confirm we show the confirm button
         proceedBtn.classList.remove('hidden');
         proceedBtn.onclick = (e) => {
             e.stopPropagation();
-            msgContainer.parentElement.classList.add('hidden');
-            callback('confirmed');
+            msgContainer.parentElement.classList.add('hidden');//closing the container
+            callback('confirmed');//returning the callback
         };
     } else {
-        proceedBtn.classList.add('hidden');
+        proceedBtn.classList.add('hidden'); //if there is no confirm we dont show confrim btn
     }
 
-    closeMsg.onclick = (e) => {
+    closeMsg.onclick = (e) => { //here its for the close btn
         e.stopPropagation();
-        msgContainer.parentElement.classList.add('hidden');
+        display('close',msgContainer.parentElement);
     };
 };
