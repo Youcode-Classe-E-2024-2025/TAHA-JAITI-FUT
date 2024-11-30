c365830bcb3de19a52387da41eb2fce0f12a68cb
Taha <kyojin.rar@gmail.com>
2024-11-30 20:58:11 +0100

    now you can switch between 4-4-2 or 4-3-3 formations dynamically

--- public/main.js
+++ public/main.js
@@ -15,55 +15,6 @@ fetchData().then(fetchedData => {
     console.log("Updated the data:", data);
 });
 
-
-// const formations = {
-//     "4-3-3": [
-//         { position: "LW", col: 1, row: 1, span: 1 },
-//         { position: "ST", col: 2, row: 1, span: 2 },
-//         { position: "RW", col: 4, row: 1, span: 1 },
-//         { position: "CM", col: 1, row: 2, span: 1 },
-//         { position: "CM", col: 2, row: 2, span: 2 },
-//         { position: "CM", col: 4, row: 2, span: 1 },
-//         { position: "LB", col: 1, row: 3, span: 1 },
-//         { position: "CB", col: 2, row: 3, span: 1 },
-//         { position: "CB", col: 3, row: 3, span: 1 },
-//         { position: "RB", col: 4, row: 3, span: 1 },
-//     ],
-//     "4-4-2": [
-//         { position: "ST", col: 2, row: 1, span: 1 },
-//         { position: "ST", col: 3, row: 1, span: 1 },
-//         { position: "LM", col: 1, row: 2, span: 1 },
-//         { position: "CM", col: 2, row: 2, span: 1 },
-//         { position: "CM", col: 3, row: 2, span: 1 },
-//         { position: "RM", col: 4, row: 2, span: 1 },
-//         { position: "LB", col: 1, row: 3, span: 1 },
-//         { position: "CB", col: 2, row: 3, span: 1 },
-//         { position: "CB", col: 3, row: 3, span: 1 },
-//         { position: "RB", col: 4, row: 3, span: 1 },
-//     ]
-// };
-
-// const changeFormation = (formationValue) => {
-//     const formation = formations[formationValue];
-//     const container = document.querySelector(".formation-container");
-
-//     container.innerHTML = "";
-
-//     formation.forEach((player) => {
-//         const playerDiv = document.createElement('div');
-
-//         let classes = `player col-start-${player.col} row-start-${player.row}`;
-
-//         if (player.span > 1) {
-//             classes += ` col-span-${player.span}`;
-//         }
-
-//         playerDiv.className = classes;
-//         playerDiv.textContent = player.position;
-//         container.appendChild(playerDiv);
-//     });
-// };
-
 const loading = document.getElementById('loadingScreen');
 
 const formationContainer = document.getElementById('formationContainer');
@@ -92,6 +43,63 @@ const changePlr = document.getElementById('changePlr');
 const editPlr = document.getElementById('editPlr');
 const deletePlr = document.getElementById('deletePlr');
 
+
+const formations = {
+    "4-3-3": [
+        { position: "LW", col: 1, row: 1, span: 1 },
+        { position: "ST", col: 2, row: 1, span: 2 },
+        { position: "RW", col: 4, row: 1, span: 1 },
+        { position: "CM", col: 1, row: 2, span: 1 },
+        { position: "CM", col: 2, row: 2, span: 2 },
+        { position: "CM", col: 4, row: 2, span: 1 },
+        { position: "LB", col: 1, row: 3, span: 1 },
+        { position: "CB", col: 2, row: 3, span: 1 },
+        { position: "CB", col: 3, row: 3, span: 1 },
+        { position: "RB", col: 4, row: 3, span: 1 },
+    ],
+    "4-4-2": [
+        { position: "ST", col: 2, row: 1, span: 1 },
+        { position: "ST", col: 3, row: 1, span: 1 },
+        { position: "LM", col: 1, row: 2, span: 1 },
+        { position: "CM", col: 2, row: 2, span: 1 },
+        { position: "CM", col: 3, row: 2, span: 1 },
+        { position: "RM", col: 4, row: 2, span: 1 },
+        { position: "LB", col: 1, row: 3, span: 1 },
+        { position: "CB", col: 2, row: 3, span: 1 },
+        { position: "CB", col: 3, row: 3, span: 1 },
+        { position: "RB", col: 4, row: 3, span: 1 },
+    ]
+};
+
+const changeFormation = (formationValue) => {
+    const formation = formations[formationValue];
+    const container = document.querySelector("#formaaaa");
+
+    container.innerHTML = "";
+
+    formation.forEach((player) => {
+        const playerDiv = document.createElement('div');
+
+        let classes = `col-start-${player.col} row-start-${player.row} emptyCard centered bg-card`;
+
+        if (player.span > 1) {
+            classes += ` col-span-${player.span}`;
+        }
+
+        playerDiv.className = classes;
+        playerDiv.innerHTML = `<span class="icon-[gg--add] text-4xl text-lime-green "></span>
+                                <p class="font-bold">${player.position}</p>`;
+
+        playerDiv.setAttribute('data-pos', player.position);
+        container.appendChild(playerDiv);
+    });
+};
+
+const formationSelect = document.getElementById('formationSelect');
+
+formationSelect.addEventListener('change', (e) => {
+    changeFormation(e.target.value);
+})
 
 let posArray = JSON.parse(localStorage.getItem('playersInTeam')) || [];
 
