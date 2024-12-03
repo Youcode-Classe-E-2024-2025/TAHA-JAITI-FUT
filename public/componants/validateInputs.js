67f2e4a28517343d77f3a5f985047782816cba4c
Taha <kyojin.rar@gmail.com>
2024-12-03 21:32:30 +0100

    accidently added the wrong file

--- public/componants/validateInputs.js
+++ public/componants/validateInputs.js
@@ -1,17 +1,15 @@
-export const validateInputs = (key,input) => {
+export const validateInputs = (key, input) => {
     if (!input.value && input.type !== 'file' && key !== 'id') {
         return `${key} can't be empty.`;
     }
     if (key === 'name' && !/^[a-zA-Z\s]{1,25}$/.test(input.value)) {
         return 'Enter a valid name (20 characters or less).';
     }
-    if (['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physical', 'rating'].includes(key)) {
-        if (input.value < 0 || input.value > 100) {
-            return `${key} must be between 0 and 100.`;
-        }
+    if (input.value < 0 || input.value > 100) {
+        return `${key} must be between 0 and 100.`;
     }
     if (key === 'position') {
-        const validPositions = ['ST', 'LW', 'RW','CM', 'RM', 'LM', 'CB', 'RB', 'LB', 'GK'];
+        const validPositions = ['ST', 'LW', 'RW', 'CM', 'RM', 'LM', 'CB', 'RB', 'LB', 'GK'];
         if (!validPositions.includes(input.value.toUpperCase())) {
             return `Select a valid position.`;
         }
